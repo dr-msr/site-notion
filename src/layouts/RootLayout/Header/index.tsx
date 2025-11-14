4d05fcb8b37867e056c3398878f24224a1e0ef1c
Dr. Syamirul <me@drmsr.dev>
2025-11-14 09:34:03 +0800

    Refactor Header component: Remove unnecessary console log and adjust padding based on scroll state for improved UI consistency.

--- src/layouts/RootLayout/Header/index.tsx
+++ src/layouts/RootLayout/Header/index.tsx
@@ -19,15 +19,11 @@ const Header: React.FC<Props> = ({ fullWidth }) => {
     return () => window.removeEventListener("scroll", handleScroll)
   }, [])
 
-  useEffect(() => {
-    console.log(isScrolled)
-  }, [isScrolled])
-
   return (
-    <div className={`z-[30] sticky top-0 bg-gray-2/50 transition-all duration-300 ease-in-out pt-4 ${isScrolled ? "pt-0 bg-gray-2/80" : ""}`}>
+    <div className={`z-[30] sticky top-0 bg-gray-2/50 transition-all duration-300 ease-in-out ${isScrolled ? "pt-0 bg-gray-2/80" : "pt-4"}`}>
       <div className="mx-auto w-full max-w-[1120px] px-4">
         <div
-          className={`flex px-2 justify-between items-center w-full h-12 rounded-2xl backdrop-blur-md shadow-sm border border-gray-4 transition-all duration-300 ease-in-out ${isScrolled ? "bg-gray-6/80 rounded-none" : "bg-white dark:bg-gray-4"} dark:shadow-none  ${
+          className={`flex px-2 justify-between items-center w-full h-12 rounded-2xl backdrop-blur-md shadow-sm transition-all duration-300 ease-in-out ${isScrolled ? "bg-gray-6/80 rounded-none border-none" : "bg-white dark:bg-gray-4 border border-gray-4"} dark:shadow-none  ${
             fullWidth ? "md:px-24" : ""
           }`}
         >
