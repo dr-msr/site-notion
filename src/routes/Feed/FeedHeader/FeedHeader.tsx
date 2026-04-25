92fcb7a8c8f21636513a568412f2972135f06371
Dr. Syamirul <me@drmsr.dev>
2026-04-25 14:01:35 +0800

    fix: remove Desc/Asc sort toggle from feed header
    Sort buttons removed entirely. Default sort order remains descending
    (newest first) via the existing default in PostList/filterPosts.

--- src/routes/Feed/FeedHeader/FeedHeader.tsx
+++ src/routes/Feed/FeedHeader/FeedHeader.tsx
@@ -1,7 +1,5 @@
-import { TCategories } from "src/types"
 import React from "react"
 import CategorySelect from "./CategorySelect"
-import OrderButtons from "./OrderButtons"
 
 type Props = {}
 
@@ -9,9 +7,6 @@ const FeedHeader: React.FC<Props> = () => {
   return (
     <div className="mb-4 border-b border-gray-6">
       <CategorySelect />
-      <div className="flex justify-end pb-2">
-        <OrderButtons />
-      </div>
     </div>
   )
 }
